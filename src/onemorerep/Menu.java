d9971197ddbee53483fe304b21ca7b74b9a0d4de
JingZhang <zjing@wustl.edu>
2021-03-26 17:24:38 -0700

    add FoodLog to the Menu

--- src/onemorerep/Menu.java
+++ src/onemorerep/Menu.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class Menu {
 
 	private Scanner keyboardIn;
+	private User user;
+	private FoodLog foodLog;
 	
 	public Menu() {
 		keyboardIn = new Scanner(System.in);
+		foodLog = new FoodLog();
 	}
 	
 	private void displayMainMenu() {
@@ -16,41 +19,118 @@ public class Menu {
 		System.out.println("Please select an option:");
 		System.out.println("1. Beginning of the day");
 		System.out.println("2. End of the day");
+		System.out.println("3. Quit");
 	}
 	
 	/*
-	 * Processing methods
+	 * Processing options from Main Menu
 	 */
 	private void processMainMenu(int selectedOption) {
 		if(selectedOption == 1) {
 			processBeginningOfTheDayMenu();
-		} else {
-			// Go to calculate Calories for the say
+		} else if(selectedOption == 2){
+			// Go to calculate Calories for the day
 			processEndOfTheDayMenu();
 		}
 	}
 	
-	
+	/*
+	 * Processing Beginning of the Day Menu
+	 * */
 	private void processBeginningOfTheDayMenu() {
 		// Go to create a user and calculate BMR
+		displayBeginningOfTheDayMenu();
+		int selectedBeginningOfTheDayMenuOption = keyboardIn.nextInt();
+		while(selectedBeginningOfTheDayMenuOption != 2) {
+			calulcateMyBMR();
+			displayBeginningOfTheDayMenu();
+			selectedBeginningOfTheDayMenuOption = keyboardIn.nextInt();
+		}
+	}
+
+	private void calulcateMyBMR() {
 		String name = getUserName();
 		int age = getUserAge();
 		double weight = getUserWeight();
 		double height = getUserHeight();
-		
-		
 		int sex = getUserSex();
 		
-		User user = new User(name, age, weight, height, sex);
-		calculateAndDisplayBMRResult(user);
+		user = new User(name, age, weight, height, sex);
+		calculateAndDisplayBMRResult();
+	}
+
+	private void displayBeginningOfTheDayMenu() {
+		System.out.println("Good Morning!");
+		System.out.println("1. Calculate my BMR");
+		System.out.println("2. Back");
 	}
 	
+	/*
+	 * Processing End of the Day Menu
+	 * */
 	private void processEndOfTheDayMenu() {
-		//To be filled out later.
+		displayEndOfTheDayMenu();
+		
+		int selectedEndOfTheDayMenuOption = keyboardIn.nextInt();
+		while(selectedEndOfTheDayMenuOption != 4) {
+			if(selectedEndOfTheDayMenuOption == 1) {
+				processFoodLogMenu();
+			} else if(selectedEndOfTheDayMenuOption == 2) {
+				System.out.println("Track your workout now!");
+				//TO DO !!!
+			} else {
+				System.out.println("View your summary of the day!");
+				//TO DO !!!
+			}
+			displayEndOfTheDayMenu();
+			selectedEndOfTheDayMenuOption = keyboardIn.nextInt();
+		}
+		
+	}
+
+	/*
+	 * Processing Food Log Menu
+	 * */
+	private void processFoodLogMenu() {
+		displayFoodLogMenu();
+		int selectedFoodLogMenuOption = keyboardIn.nextInt();
+		while(selectedFoodLogMenuOption != 3) {
+			if(selectedFoodLogMenuOption == 1) {
+				addFoodToFoodLog();
+			} else if(selectedFoodLogMenuOption == 2) {
+				foodLog.displayFoodLog();
+			}
+			displayFoodLogMenu();
+			selectedFoodLogMenuOption = keyboardIn.nextInt();
+		}
+	}
+
+	private void addFoodToFoodLog() {
+		System.out.println("Please enter food name");
+		String name = keyboardIn.next();
+		System.out.println("Please enter food calories");
+		int calories = keyboardIn.nextInt();
+		Food food = new Food(name, calories);
+		foodLog.addFood(food);
+	}
+
+	private void displayFoodLogMenu() {
+		System.out.println("Track your food intake now!");
+		System.out.println("1. Enter a food");
+		System.out.println("2. View food log summary");
+		System.out.println("3. Back");
+	}
+
+	private void displayEndOfTheDayMenu() {
+		System.out.println("Good evening!");
+		System.out.println("1. Calculate calories taken from food");
+		System.out.println("2. Calculate calories burned from workout");
+		System.out.println("3. Summary of the day");
+		System.out.println("4. Back");
 	}
 	
 	
-	private void calculateAndDisplayBMRResult(User user) {
+	private void calculateAndDisplayBMRResult() {
 		int bmr = user.calculateBMR();
 		System.out.println("Your BMR is: " + bmr);
 	}
@@ -110,7 +190,11 @@ public class Menu {
 		
 		int selectedOption = this.getUserStartOrEndOfDayOption();
 		
-		this.processMainMenu(selectedOption);
+		while(selectedOption != 3) {
+			this.processMainMenu(selectedOption);
+			this.displayMainMenu();
+			selectedOption = this.getUserStartOrEndOfDayOption();
+		}
 		
 		this.keyboardIn.close();
 	}
